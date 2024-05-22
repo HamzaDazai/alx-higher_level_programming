1d642ba4720622de5bd4ed13bfcd572a8d426201
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-05-22 21:31:13 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,4 @@
+-- Creates the database hbtn_0d_usa with the table cities.
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
 CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     PRIMARY KEY(`id`),
@@ -6,4 +7,4 @@ CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
     `name`     VARCHAR(256) NOT NULL,
     FOREIGN KEY(`state_id`)
     REFERENCES `hbtn_0d_usa`.`states`(`id`)
-)
+);
