e2df81091baa6454fbd321712ac20bcba6ab1812
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-05-22 21:14:20 +0100

    Create 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,6 @@
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+       WHERE g.`genre_id` IS NULL
+ ORDER BY s.`title`, g.`genre_id`;
